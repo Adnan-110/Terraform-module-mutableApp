f2ceef0ca82eda48414293cb11168d59d03939ea
Adnan <adnanshaikh11501@gmail.com>
2024-02-20 09:31:44 +0530

    Node Exporter

--- securityGroup.tf
+++ securityGroup.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "allows_app" {
         protocol    = "tcp"
         cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR, data.terraform_remote_state.vpc.outputs.VPC_CIDR]
     }
+
+    ingress {
+    description      = "Node Exporter Port"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
     egress {
         from_port   = 0
         to_port     = 0
